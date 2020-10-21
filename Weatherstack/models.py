f4a155750e552ac2c46028239779c5e3860bb2cd
ravi0497 <muhammadravi04@gmail.com>
2020-10-21 18:51:02 +0700

    Daily transformation

--- Weatherstack/models.py
+++ Weatherstack/models.py
@@ -5,17 +5,33 @@ import pandas as pd
 
 class Batch():
 
-    def get_historical(client,access_key,cities,date,hour):
+    def get_historical(client,access_key,cities,date,hourly):
         query={'access_key':access_key,'query':cities,
-                'historical_date':date,'hourly':hour}
+                'historical_date':date,'hourly':hourly}
 
         response = requests.get("http://api.weatherstack.com/historical?",params=query)
+        if(response.status_code==200):
+            json_file=response.json()
+            try:
+                daily_list=[]
+                historical = json_file['historical']
+                location = json_file['location']
+                for x in historical:
+                    historical[x]['location']=location
+                    daily_list.append(historical[x])
 
-        filepath='{}/{}.json'.format(client,'data')
-        with open(filepath,'w') as fp:
-            json.dump(response.json(),fp)
+                result=pd.json_normalize(daily_list,'hourly',
+                ['date','date_epoch','mintemp','maxtemp','avgtemp','totalsnow','sunhour','astro','location'])
 
-        return 'Historical weather data from {} has been added to {}'.format(cities,client)
+                result.to_csv('{}'.format(client),index=False)
+                return 'Daily weather data from {} has been added to {}'.format(cities,client)
+
+            except:
+                return json_file['error']['info']
+        else:
+            return 'Error. Check Again Your Input'
+
+
 
     def get_time_series(client,access_key,cities,historical_date_start,historical_date_end,hourly):
         query={'access_key':access_key,'query':cities,
