ad6e98ffb471592304950627d895436f3c58829b
ravi0497 <muhammadravi04@gmail.com>
2020-10-27 21:53:22 +0700

    twitter model

--- twitter/models.py
+++ twitter/models.py
@@ -40,26 +40,36 @@ class Twitter():
         except:
             import traceback
             traceback.print_exc()
-            return 'Something went wrong, quitting...'
+            return ['Something went wrong, quitting...']
 
 
     def normalize_users(follower,following,username,path):
-        try:
-            follower_dicts=[]
-            following_dicts=[]
+        # try:
+        follower_dicts=[]
+        following_dicts=[]
 
-            for x in follower_dicts:
-                follower_dicts.append(x.__dict__)
+        for x in follower['fullusers']:
+            follower_dicts.append(x.__dict__)
 
-            for x in following_dicts:
-                following_dicts.append(x.__dict__)
+        for x in following['fullusers']:
+            following_dicts.append(x.__dict__)
 
-            follower_csv=pd.json_normalize(follower_dicts)
-            following_csv=pd.json_normalize(following_dicts)
+        follower_csv=pd.json_normalize(follower_dicts)
+        following_csv=pd.json_normalize(following_dicts)
 
-            follower_csv.to_csv('{}\{}_follower.csv'.format(path,username,username))
-            following_csv.to_csv('{}\{}_following.csv'.format(path,username,username))
+        follower_csv = follower_csv[['id','id_str','name','screen_name','location','description',
+                      'url','protected','followers_count','friends_count','listed_count',
+                      'created_at','favourites_count','utc_offset','time_zone','geo_enabled',
+                      'verified','statuses_count', 'lang','status','profile_image_url_https']]
 
-            return '{} has {} followers and {} friends. Data has been saved to {}'.format(username,follower['u_count'],following['u_count'],path)
-        except:
-            return "Limit have been reached wait for 15 minutes"
+        following_csv = following_csv[['id','id_str','name','screen_name','location','description',
+                      'url','protected','followers_count','friends_count','listed_count',
+                      'created_at','favourites_count','utc_offset','time_zone','geo_enabled',
+                      'verified','statuses_count', 'lang','status','profile_image_url_https']]
+
+        follower_csv.to_csv('{}\{}_follower.csv'.format(path,username,username))
+        following_csv.to_csv('{}\{}_following.csv'.format(path,username,username))
+
+        return '{} has {} followers and {} friends. Data has been saved to {}'.format(username,follower['u_count'],following['u_count'],path)
+        # except:
+        #     return "Limit have been reached wait for 15 minutes"
